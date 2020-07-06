45ffafe7740fa905e99180fe9e4989c1e939bfd9
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2020-07-06 20:45:28 +1000

    IGSN-90. Removed Schema Entity

--- src/main/java/au/edu/ardc/igsn/repository/SchemaRepository.java
+++
@@ -1,9 +0,0 @@
-package au.edu.ardc.igsn.repository;
-
-import au.edu.ardc.igsn.entity.SchemaEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SchemaRepository extends PagingAndSortingRepository<SchemaEntity, String> {
-}
