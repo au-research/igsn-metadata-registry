1ca00951b73d7f0240699bee691d7cf93fd2fa40
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2020-09-22 08:50:49 +1000

    Refactor SchemaNotSupportedException

--- src/main/java/au/edu/ardc/registry/exception/SchemaNotSupportedException.java
+++ src/main/java/au/edu/ardc/registry/exception/SchemaNotSupportedException.java
@@ -1,9 +1,21 @@
 package au.edu.ardc.registry.exception;
 
-public class SchemaNotSupportedException extends RuntimeException {
+public class SchemaNotSupportedException extends APIException {
+
+	private final String schemaID;
 
 	public SchemaNotSupportedException(String schemaID) {
-		super(String.format("Schema %s is not supported", schemaID));
+		super();
+		this.schemaID = schemaID;
 	}
 
+	@Override
+	public String[] getArgs() {
+		return new String[] { this.schemaID };
+	}
+
+	@Override
+	public String getMessageID() {
+		return "api.error.schema-not-supported";
+	}
 }
