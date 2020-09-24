8627d26f9ec6387f9097ed685ad7cefd8a91cd0d
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2020-09-24 13:38:42 +1000

    IGSN-90. Added RecordService tests for improved coverage

--- src/test/java/au/edu/ardc/registry/common/service/RecordServiceTest.java
+++ src/test/java/au/edu/ardc/registry/common/service/RecordServiceTest.java
@@ -4,14 +4,14 @@ import au.edu.ardc.registry.TestHelper;
 import au.edu.ardc.registry.common.dto.RecordDTO;
 import au.edu.ardc.registry.common.dto.mapper.RecordMapper;
 import au.edu.ardc.registry.common.entity.Record;
-import au.edu.ardc.registry.exception.ForbiddenOperationException;
-import au.edu.ardc.registry.exception.RecordNotFoundException;
 import au.edu.ardc.registry.common.model.Allocation;
 import au.edu.ardc.registry.common.model.DataCenter;
 import au.edu.ardc.registry.common.model.Scope;
 import au.edu.ardc.registry.common.model.User;
 import au.edu.ardc.registry.common.repository.RecordRepository;
 import au.edu.ardc.registry.common.repository.specs.RecordSpecification;
+import au.edu.ardc.registry.exception.ForbiddenOperationException;
+import au.edu.ardc.registry.exception.RecordNotFoundException;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -20,8 +20,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
@@ -29,7 +27,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -39,23 +40,24 @@ import static org.mockito.Mockito.*;
 @ContextConfiguration(classes = { RecordService.class, RecordMapper.class, ModelMapper.class })
 public class RecordServiceTest {
 
+	@MockBean
+	ValidationService validationService;
+
 	@Autowired
 	private RecordService service;
 
 	@MockBean
 	private RecordRepository repository;
 
-	@MockBean
-	ValidationService validationService;
-
 	@Test
-	@DisplayName("search will pass along to repository.findAll(specs, pageable)")
+	@DisplayName("search will call repository.findAll(specs, pageable)")
 	void search() {
 		service.search(new RecordSpecification(), PageRequest.of(0, 10));
 		verify(repository, times(1)).findAll(any(RecordSpecification.class), any(Pageable.class));
 	}
 
 	@Test
+	@DisplayName("findOwnedById given an existed record with correct validation should return record")
 	void findOwnedById_recordExists_returnRecord() {
 		// given a record & user
 		Record record = TestHelper.mockRecord(UUID.randomUUID());
@@ -75,6 +77,30 @@ public class RecordServiceTest {
 	}
 
 	@Test
+	@DisplayName("Throws Exception when findOwnedById ownership validation failed")
+	void findOwnedById_recordNotOwned_throwsException() {
+		// given a record & user
+		Record record = TestHelper.mockRecord(UUID.randomUUID());
+		User user = TestHelper.mockUser();
+
+		// mock repository
+		when(repository.findById(record.getId())).thenReturn(Optional.of(record));
+		when(validationService.validateRecordOwnership(record, user)).thenReturn(false);
+
+		// when findOwnedById
+		Assert.assertThrows(ForbiddenOperationException.class,
+				() -> service.findOwnedById(record.getId().toString(), user));
+	}
+
+	@Test
+	@DisplayName("Saving record calls repository.saveAndFlush")
+	void save() {
+		service.save(TestHelper.mockRecord());
+		verify(repository, times(1)).saveAndFlush(any(Record.class));
+	}
+
+	@Test
+	@DisplayName("findById throws Exception when record doesn't exist")
 	public void findById_recordDoesNotExist_ExceptionRecordNotFound() {
 		// given a record & a user
 		Record record = TestHelper.mockRecord(UUID.randomUUID());
@@ -84,12 +110,12 @@ public class RecordServiceTest {
 		when(repository.findById(record.getId())).thenReturn(Optional.empty());
 
 		// when findById expects RecordNotFoundException
-		Assertions.assertThrows(RecordNotFoundException.class, () -> {
-			service.findOwnedById(record.getId().toString(), user);
-		});
+		Assertions.assertThrows(RecordNotFoundException.class,
+				() -> service.findOwnedById(record.getId().toString(), user));
 	}
 
 	@Test
+	@DisplayName("exists calls repository.existsById")
 	public void exists_recordExist_returnTrue() {
 		UUID randomUUID = UUID.randomUUID();
 		when(repository.existsById(randomUUID)).thenReturn(true);
@@ -97,6 +123,7 @@ public class RecordServiceTest {
 	}
 
 	@Test
+	@DisplayName("exists returns false when record doesn't exist")
 	public void exists_recordDoesNotExist_returnFalse() {
 		UUID randomUUID = UUID.randomUUID();
 		when(repository.existsById(randomUUID)).thenReturn(false);
@@ -104,6 +131,7 @@ public class RecordServiceTest {
 	}
 
 	@Test
+	@DisplayName("create throws exception when user doesn't have access to allocation")
 	public void create_UserDoesNotHaveAccessToAllocation_throwsException() {
 		// given a User with the wrong permission
 		User user = TestHelper.mockUser();
@@ -112,36 +140,34 @@ public class RecordServiceTest {
 		RecordDTO dto = new RecordDTO();
 
 		// expects ForbiddenOperationException
-		Assertions.assertThrows(ForbiddenOperationException.class, () -> {
-			service.create(dto, user);
-		});
+		Assertions.assertThrows(ForbiddenOperationException.class, () -> service.create(dto, user));
 	}
 
 	@Test
+	@DisplayName("create throws exception when user has access to allocation but not the right scope")
 	public void create_UserDoesNotHaveScope_throwsException() {
 		// given a User with the wrong scope
 		User user = TestHelper.mockUser();
 		Allocation allocation = new Allocation(UUID.randomUUID());
-		allocation.setScopes(Arrays.asList(Scope.UPDATE));
-		user.setAllocations(Arrays.asList(allocation));
+		allocation.setScopes(Collections.singletonList(Scope.UPDATE));
+		user.setAllocations(Collections.singletonList(allocation));
 
 		// given a dto
 		RecordDTO dto = new RecordDTO();
 		dto.setAllocationID(allocation.getId());
 
 		// expects ForbiddenOperationException
-		Assertions.assertThrows(ForbiddenOperationException.class, () -> {
-			service.create(dto, user);
-		});
+		Assertions.assertThrows(ForbiddenOperationException.class, () -> service.create(dto, user));
 	}
 
 	@Test
-	public void create_UserHasSufficientPermission_returnsDTO() {
+	@DisplayName("create returns correct record when user has sufficient permissions")
+	public void create_UserHasSufficientPermission_returnsRecord() {
 		// given a User with the right permission
 		User user = TestHelper.mockUser();
 		Allocation allocation = new Allocation(UUID.randomUUID());
-		allocation.setScopes(Arrays.asList(Scope.CREATE));
-		user.setAllocations(Arrays.asList(allocation));
+		allocation.setScopes(Collections.singletonList(Scope.CREATE));
+		user.setAllocations(Collections.singletonList(allocation));
 
 		// given a dto
 		RecordDTO dto = new RecordDTO();
@@ -150,7 +176,8 @@ public class RecordServiceTest {
 		// setup repository mock
 		Record expected = TestHelper.mockRecord(UUID.randomUUID());
 		when(repository.save(any(Record.class))).thenReturn(expected);
-		when(validationService.validateAllocationScope(any(Allocation.class), eq(user), eq(Scope.CREATE))).thenReturn(true);
+		when(validationService.validateAllocationScope(any(Allocation.class), eq(user), eq(Scope.CREATE)))
+				.thenReturn(true);
 
 		// when the service creates the record with the dto and the user
 		Record result = service.create(dto, user);
@@ -162,6 +189,44 @@ public class RecordServiceTest {
 	}
 
 	@Test
+	@DisplayName("create record and overwrite correctly when user has elevated import permission")
+	void create_UserHasImportPermission_returnsRecordOverwritten() {
+		// given a User with the right permission
+		User user = TestHelper.mockUser();
+		Allocation allocation = new Allocation(UUID.randomUUID());
+		allocation.setScopes(Collections.singletonList(Scope.CREATE));
+		user.setAllocations(Collections.singletonList(allocation));
+
+		// given a dto
+		RecordDTO dto = new RecordDTO();
+		dto.setAllocationID(allocation.getId());
+
+		Record expected = TestHelper.mockRecord(UUID.randomUUID());
+
+		when(repository.save(any(Record.class))).thenReturn(expected);
+		when(validationService.validateAllocationScope(any(Allocation.class), eq(user), eq(Scope.CREATE)))
+				.thenReturn(true);
+		when(validationService.validateAllocationScope(any(Allocation.class), eq(user), eq(Scope.IMPORT)))
+				.thenReturn(true);
+
+		Record result = service.create(dto, user);
+
+		assertThat(result).isInstanceOf(Record.class);
+		assertThat(result.getId()).isNotNull();
+		verify(repository, times(1)).save(any(Record.class));
+	}
+
+	@Test
+	@DisplayName("delete throws exception when record doesn't exist")
+	void delete_NotExist_throwsException() {
+		Record record = TestHelper.mockRecord(UUID.randomUUID());
+		when(repository.existsById(record.getId())).thenReturn(false);
+		Assertions.assertThrows(RecordNotFoundException.class,
+				() -> service.delete(UUID.randomUUID().toString(), TestHelper.mockUser()));
+	}
+
+	@Test
+	@DisplayName("delete throws exception when the user doesn't own the record")
 	public void delete_NotOwned_throwsException() {
 		// given a User with the wrong permission
 		User user = TestHelper.mockUser();
@@ -176,12 +241,12 @@ public class RecordServiceTest {
 		when(repository.findById(record.getId())).thenReturn(Optional.of(record));
 
 		// expects ForbiddenOperationException
-		Assertions.assertThrows(ForbiddenOperationException.class, () -> {
-			service.delete(record.getId().toString(), user);
-		});
+		Assertions.assertThrows(ForbiddenOperationException.class,
+				() -> service.delete(record.getId().toString(), user));
 	}
 
 	@Test
+	@DisplayName("delete returns true when the user owns the record")
 	public void delete_UserSufficientPermission_returnsTrue() {
 		// given a User with the wrong permission
 		User user = TestHelper.mockUser();
@@ -204,18 +269,18 @@ public class RecordServiceTest {
 	}
 
 	@Test
+	@DisplayName("update throws exception when record doesn't exist")
 	public void update_RecordDoesNotExist_throwsException() {
 		// given a random dto
 		RecordDTO dto = new RecordDTO();
 		dto.setId(UUID.randomUUID());
 
 		// when update with a random user expects RecordNotFoundException
-		Assertions.assertThrows(RecordNotFoundException.class, () -> {
-			service.update(dto, TestHelper.mockUser());
-		});
+		Assertions.assertThrows(RecordNotFoundException.class, () -> service.update(dto, TestHelper.mockUser()));
 	}
 
 	@Test
+	@DisplayName("update throws exception when user doesn't own the record")
 	public void update_OwnerTypeUserUserDoesNotHavePermission_throwsException() {
 		// an existing record
 		Record record = TestHelper.mockRecord(UUID.randomUUID());
@@ -234,12 +299,11 @@ public class RecordServiceTest {
 		when(repository.findById(any(UUID.class))).thenReturn(Optional.of(record));
 
 		// expects ForbiddenOperationException
-		Assertions.assertThrows(ForbiddenOperationException.class, () -> {
-			service.update(dto, user);
-		});
+		Assertions.assertThrows(ForbiddenOperationException.class, () -> service.update(dto, user));
 	}
 
 	@Test
+	@DisplayName("update throws exception when record is owned by a datacenter and the user doesn't have that group")
 	public void update_OnwerTypeDataCenter_throwsException() {
 		// an existing record
 		Record record = TestHelper.mockRecord(UUID.randomUUID());
@@ -258,13 +322,12 @@ public class RecordServiceTest {
 		when(repository.findById(any(UUID.class))).thenReturn(Optional.of(record));
 
 		// expects ForbiddenOperationException
-		Assertions.assertThrows(ForbiddenOperationException.class, () -> {
-			service.update(dto, user);
-		});
+		Assertions.assertThrows(ForbiddenOperationException.class, () -> service.update(dto, user));
 	}
 
 	@Test
-	public void update_OwnerTypeUser_returnsDTO() {
+	@DisplayName("update returns record when the user owns the record privately")
+	public void update_OwnerTypeUser_returnsRecord() {
 		// given a User
 		User user = TestHelper.mockUser();
 
@@ -289,13 +352,14 @@ public class RecordServiceTest {
 	}
 
 	@Test
-	public void update_OnwerTypeDataCenter_returnsDTO() {
+	@DisplayName("update returns record when the user owns the record via data center")
+	public void update_OwnerTypeDataCenter_returnsDTO() {
 		// datacenter
 		DataCenter dataCenter = new DataCenter(UUID.randomUUID());
 
 		// given a User
 		User user = TestHelper.mockUser();
-		user.setDataCenters(Arrays.asList(dataCenter));
+		user.setDataCenters(Collections.singletonList(dataCenter));
 
 		// owns an existing record
 		Record record = TestHelper.mockRecord(UUID.randomUUID());
@@ -318,7 +382,8 @@ public class RecordServiceTest {
 	}
 
 	@Test
-	public void update_UserImportScope_returnsDTO() throws ParseException {
+	@DisplayName("update returns overwritten record when the user has import elevated permission")
+	public void update_UserImportScope_returnsRecord() throws ParseException {
 		// given a User with the import scope to the record
 		User user = TestHelper.mockUser();
 		Allocation allocation = new Allocation(UUID.randomUUID());
@@ -342,6 +407,8 @@ public class RecordServiceTest {
 		when(repository.findById(dto.getId())).thenReturn(Optional.of(record));
 		when(repository.save(any(Record.class))).thenReturn(TestHelper.mockRecord());
 		when(validationService.validateRecordOwnership(any(Record.class), eq(user))).thenReturn(true);
+		when(validationService.validateAllocationScope(any(Allocation.class), eq(user), eq(Scope.IMPORT)))
+				.thenReturn(true);
 
 		Record expected = service.update(dto, user);
 		assertThat(expected).isInstanceOf(Record.class);
@@ -349,17 +416,17 @@ public class RecordServiceTest {
 	}
 
 	@Test
+	@DisplayName("findPublicById throws exception when record doesn't exist")
 	void findPublicById_notfound_throwsException() {
 		// given no record
 		when(repository.findById(anyString())).thenReturn(null);
 
 		// when findPublicById, throws Exception
-		Assert.assertThrows(RecordNotFoundException.class, () -> {
-			service.findPublicById(UUID.randomUUID().toString());
-		});
+		Assert.assertThrows(RecordNotFoundException.class, () -> service.findPublicById(UUID.randomUUID().toString()));
 	}
 
 	@Test
+	@DisplayName("findPublicById throws exception when record is not visible")
 	void findPublicById_privateRecord_throwsException() {
 		// given a private record
 		Record record = TestHelper.mockRecord();
@@ -367,12 +434,11 @@ public class RecordServiceTest {
 		when(repository.findById(anyString())).thenReturn(Optional.of(record));
 
 		// when findPublicById, throws Exception
-		Assert.assertThrows(RecordNotFoundException.class, () -> {
-			service.findPublicById(UUID.randomUUID().toString());
-		});
+		Assert.assertThrows(RecordNotFoundException.class, () -> service.findPublicById(UUID.randomUUID().toString()));
 	}
 
 	@Test
+	@DisplayName("findPublicById return record when record is public")
 	void findPublicById_foundRecord_returns() {
 		// given a public record
 		Record expected = TestHelper.mockRecord(UUID.randomUUID());
