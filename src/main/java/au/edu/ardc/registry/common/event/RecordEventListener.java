3554a1e5d61fc315d584a15094013dc2bf0feafe
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2020-10-08 14:26:38 +1100

    IGSN-95. ProcessRecordJob is now run for every RecordUpdatedEvent dispatched

--- src/main/java/au/edu/ardc/registry/common/event/RecordEventListener.java
+++ src/main/java/au/edu/ardc/registry/common/event/RecordEventListener.java
@@ -1,15 +1,21 @@
 package au.edu.ardc.registry.common.event;
 
-import au.edu.ardc.registry.common.entity.Record;
-import au.edu.ardc.registry.common.service.SchemaService;
-import au.edu.ardc.registry.job.processor.RecordTitleProcessor;
 import au.edu.ardc.registry.common.service.RecordService;
+import au.edu.ardc.registry.common.service.SchemaService;
 import au.edu.ardc.registry.common.service.VersionService;
+import au.edu.ardc.registry.job.processor.RecordTitleProcessor;
 import au.edu.ardc.registry.job.processor.RecordTransformLDProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -22,32 +28,26 @@ public class RecordEventListener {
 	Logger logger = LoggerFactory.getLogger(RecordEventListener.class);
 
 	@Autowired
-	JobLauncher jobLauncher;
+	@Qualifier("ProcessRecordJob")
+	Job ProcessRecordJob;
 
 	@Autowired
-	VersionService versionService;
-
-	@Autowired
-	RecordService recordService;
-
-	@Autowired
-	SchemaService schemaService;
+	@Qualifier("queueJobLauncher")
+	JobLauncher queue;
 
 	@Async
 	@EventListener
-	public void handleRecordUpdated(RecordUpdatedEvent event) throws InterruptedException {
+	public void handleRecordUpdated(RecordUpdatedEvent event) throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
 		logger.debug("Event RecordUpdatedEvent raised with record {} and user {}", event.getRecord().getId(),
 				event.getUser().getId());
 		event.getRecord().setModifiedAt(new Date());
 		event.getRecord().setModifierID(event.getUser().getId());
 
-		// todo ProcessRecordJob (for single record)
-		RecordTitleProcessor titleProcessor = new RecordTitleProcessor(versionService, recordService, schemaService);
-		titleProcessor.process(event.getRecord());
+		// trigger a new job
+		JobParametersBuilder paramBuilder = new JobParametersBuilder().addString("method", "findById")
+				.addString("id", event.getRecord().getId().toString()).addLong("time", System.currentTimeMillis());
 
-		RecordTransformLDProcessor transformProcessor = new RecordTransformLDProcessor(versionService, recordService,
-				schemaService);
-		transformProcessor.process(event.getRecord());
+		queue.run(ProcessRecordJob, paramBuilder.toJobParameters());
 	}
 
 }
