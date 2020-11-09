9447000b167d4d0a9c55ac13673ce4619d45883d
Liz Woods <lwoods@ANDSs-MacBook-Pro.local>
2020-11-09 12:07:05 +1100

    IGSN-152 removed api/services/getIdentifierStatus and added api/services/getVersionStatus to be accessed by portal

--- src/main/java/au/edu/ardc/registry/common/controller/api/services/AuthenticationCheckController.java
+++ src/main/java/au/edu/ardc/registry/common/controller/api/services/AuthenticationCheckController.java
@@ -3,19 +3,20 @@ package au.edu.ardc.registry.common.controller.api.services;
 import au.edu.ardc.registry.common.entity.Embargo;
 import au.edu.ardc.registry.common.entity.Identifier;
 import au.edu.ardc.registry.common.entity.Record;
-import au.edu.ardc.registry.common.service.EmbargoService;
+import au.edu.ardc.registry.common.entity.Version;
+import au.edu.ardc.registry.common.service.*;
+import au.edu.ardc.registry.common.util.XMLUtil;
 import au.edu.ardc.registry.exception.ForbiddenOperationException;
 import au.edu.ardc.registry.common.model.User;
-import au.edu.ardc.registry.common.service.IdentifierService;
-import au.edu.ardc.registry.common.service.KeycloakService;
-import au.edu.ardc.registry.common.service.ValidationService;
 import au.edu.ardc.registry.igsn.exception.IGSNNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class AuthenticationCheckController {
@@ -26,14 +27,17 @@ public class AuthenticationCheckController {
 
 	EmbargoService embargoService;
 
+	VersionService versionService;
+
 	KeycloakService kcService;
 
 	public AuthenticationCheckController(IdentifierService identifierService, ValidationService validationService,
-			KeycloakService kcService, EmbargoService embargoService) {
+			KeycloakService kcService, EmbargoService embargoService, VersionService versionService) {
 		this.identifierService = identifierService;
 		this.validationService = validationService;
 		this.kcService = kcService;
 		this.embargoService = embargoService;
+		this.versionService = versionService;
 	}
 
 	@GetMapping("/api/services/auth-check")
@@ -70,8 +74,7 @@ public class AuthenticationCheckController {
 	}
 
 	@GetMapping("/api/services/hasEmbargo")
-	public ResponseEntity<?> hasEmbargo(@RequestParam(name = "identifier") String identifierValue,
-												  HttpServletRequest request) {
+	public ResponseEntity<?> hasEmbargo(@RequestParam(name = "identifier") String identifierValue) {
 		Identifier identifier = identifierService.findByValueAndType(identifierValue, Identifier.Type.IGSN);
 		if (identifier == null) {
 			throw new IGSNNotFoundException(identifierValue);
@@ -84,14 +87,25 @@ public class AuthenticationCheckController {
 		return ResponseEntity.ok().body(embargo.getEmbargoEnd().toString());
 	}
 
-	@GetMapping("/api/services/getIdentifierStatus")
-	public ResponseEntity<?> getIdentifierStatus(@RequestParam(name = "identifier") String identifierValue,
-										HttpServletRequest request) {
+	@GetMapping("/api/services/getVersionStatus")
+	public ResponseEntity<?> getVersionStatus(@RequestParam(name = "identifier") String identifierValue,
+											  @RequestParam(name = "schema") String schema) {
 		Identifier identifier = identifierService.findByValueAndType(identifierValue, Identifier.Type.IGSN);
 		if (identifier == null) {
 			throw new IGSNNotFoundException(identifierValue);
 		}
-		return ResponseEntity.ok().body(identifier.getStatus().toString());
+		String result;
+		Record record = identifier.getRecord();
+		Version version = versionService.findVersionForRecord(record, schema);
+		String contentString =  new String(version.getContent(), StandardCharsets.US_ASCII);
+		try {
+			NodeList nodeList = XMLUtil.getXPath(contentString, "//logDate");
+			Element versionLogDate = (Element) nodeList.item(0);
+			result = versionLogDate.getAttribute("eventType");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			result = null;
+		}
+		return ResponseEntity.ok().body(result);
 	}
-
 }
